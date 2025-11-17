098e63faf2d04dba2e3dcefe2e6945065f2382c1
Igor Cretu <igorcretu09@gmail.com>
2025-11-18 00:20:24 +0100

    build fix 2

--- src/pages/CarDetail.tsx
+++ src/pages/CarDetail.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { ArrowLeft, Calendar, Fuel, Gauge, MapPin, Car as CarIcon, TrendingUp, Loader } from 'lucide-react';
+import { ArrowLeft, Fuel, Gauge, Car as CarIcon, TrendingUp, Loader } from 'lucide-react';
 import api from '../api/client';
 
 interface CarDetail {
